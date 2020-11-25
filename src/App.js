64c4745c9691a9e23a74c192f2e6f9b1fc84b2dd
Ahsan Jamil <57091356+ahsanjamil306@users.noreply.github.com>
2020-11-25 19:12:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Child from './child.js';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   
+   <div className="App"> 
+   <Child name="AHSAN JAMIL" designation="Software Engineer"/>
+   <hr/>
+   <Child name="SAAd MURTAZA" designation="PETROLEUM ENGINEER"/>
+   <hr/>
+   
+   </div>
+ );
 }
 
 export default App;
